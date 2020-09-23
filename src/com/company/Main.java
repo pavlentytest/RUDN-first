55804102665ea451dff65621046b5b8e6a924c79
Your Name <you@example.com>
2020-09-23 20:15:13 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+        String c = "hello";
+        char ch = 'A';
+        byte h = 1;
+        long f = 100;
+        double dd = 8.9;
+        float d = 5.67f;
+        a = (int)dd; //8
+        dd = b; // 200.0
+
+        int z = 2/3; // 0
+        int zz = 2%3; // 2
+
+        double zzz = 2/3;
+
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        scanner.
+
+
+        System.out.println(a+b);
+    }
+}
